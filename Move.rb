94989e015bf0243bba6a18c0a5f7a47ffe7efba4
prajitbhandari <prajitbhandari@gmail.com>
2019-10-15 16:04:53 +0545

    Used Rubocop

--- Move.rb
+++ Move.rb
@@ -1,8 +1,8 @@
 class  Move
-    attr_accessor :row,:col
-    
-    def initialize(x,y) 
-        @row=x
-        @col=y
-    end
+  attr_accessor :row, :col
+
+  def initialize(x, y) 
+    @row = x
+    @col = y
+  end
 end
--- Player.rb
+++ Player.rb
@@ -1,25 +1,18 @@
 require_relative 'Move.rb'
-
 class Player
-    attr_accessor :identifier,:move_token
-    
-    def initialize(identifier,move_token)
-        
-        @identifier=identifier
-        @move_token=move_token
-    end
-    
-    def askNextMove
-        # newUserMove //
-        puts "Enter the move_mark at available Array index [][]"
-        puts " "
-        @row= (gets.chomp).to_i
-        @col= (gets.chomp).to_i
-        @obj= Move.new(@row,@col)
-        # registerMove(@obj)
-    end
-       
-    # def registerMove(obj)
-    #      @board[obj.row][obj.col]=self.move_token
-    # end
+  attr_accessor :identifier, :move_token
+  
+  def initialize(identifier, move_token)       
+    @identifier=identifier
+    @move_token=move_token
+  end
+
+  def askNextMove
+    puts 'Enter the move_mark at available Array index [][]'
+    puts 
+    @row = gets.chomp.to_i
+    @col = gets.chomp.to_i
+    @obj = Move.new(@row, @col)
+  end
+
 end
